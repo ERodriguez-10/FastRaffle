2d98f2904f782d275fac1b0aaa3152c5ef976513
Mariano Botto <marianobotto@gmail.com>
2024-03-12 14:54:11 -0300

    feature - Front: Add Navigation.

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home/Home.jsx";
+import Login from "./pages/Login/Login.jsx";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
