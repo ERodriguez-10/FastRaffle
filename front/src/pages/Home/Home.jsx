5c02a5fe675fffa73b4e3491436407f251121193
Mariano Botto <marianobotto@gmail.com>
2024-03-12 15:13:06 -0300

    fix - Front: Fix navigation

--- front/src/pages/Home/Home.jsx
+++ front/src/pages/Home/Home.jsx
@@ -1,11 +1,8 @@
 import "./Home.css";
 import Button from "../../components/Button/Button.jsx";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  const handleNavegation = () => {
-    window.location.href = "/login";
-  };
-
   return (
     <div className="container mx-auto">
       <img
@@ -18,12 +15,13 @@ const Home = () => {
       <h2 className="text-center text-white text-3xl">
         ¡La app de sorteos de DevTalles!
       </h2>
-      <Button
-        text={"Comenzar"}
-        bg={"#ffa988"}
-        className={"my-8 px-10 py-2 rounded-md font-bold"}
-        onClickFunction={handleNavegation}
-      />
+      <Link to={"/login"}>
+        <Button
+          text={"Comenzar"}
+          bg={"#ffa988"}
+          className={"my-8 px-10 py-2 rounded-md font-bold"}
+        />
+      </Link>
       <div className="text-left border-gray-400 w-1/3 border px-3 py-3 rounded-md">
         <h4 className="text-xl text-white pb-6">
           Participa, comparte y disfruta!
