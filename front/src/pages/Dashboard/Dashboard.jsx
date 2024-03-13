0124c0e5019b702a908dd74ec330744c3f6f2f20
Mariano Botto <marianobotto@gmail.com>
2024-03-13 14:43:35 -0300

    feature - Front: Add Modal

--- front/src/pages/Dashboard/Dashboard.jsx
+++ front/src/pages/Dashboard/Dashboard.jsx
@@ -1,68 +1,77 @@
+import { useState } from "react"
 import Button from "../../components/Button/Button.jsx"
 import Card from "../../components/Card/Card.jsx"
+import Modal from "../../components/Modal/Modal.jsx"
 
 const Dashboard = () => {
+  const [isOpenModal, setIsOpenModal] = useState(false)
+
   return (
-    <div className="container mx-auto">
-      <div className="flex justify-between text-white" style={{width: '100%'}}>
-        <h1 className="my-auto font-bold text-2xl" style={{background: 'transparent', display: 'inline'}}>Dashboard</h1>
-        <div className="">
-          <Button           
-            text={"Creator view"}
-            bg={"transparent"}
-            iconName={"ri-admin-line"}
-            className={"my-8 mx-2 px-6 py-2 rounded-md font-bold border border-gray-400"}
-          />
-          <Button           
-            text={"Enter hackathon code"}
-            bg={"#ffa988"}
-            iconName={"ri-add-circle-line"}
-            className={"my-8 mx-2 px-6 py-2 rounded-md font-bold text-black"}
+    <>
+      {isOpenModal ? <Modal setIsOpenModal={setIsOpenModal}/> : <></>}
+      <div className="container mx-auto">
+        <div className="flex justify-between text-white" style={{width: '100%'}}>
+          <h1 className="my-auto font-bold text-2xl" style={{background: 'transparent', display: 'inline'}}>Dashboard</h1>
+          <div className="">
+            <Button           
+              text={"Creator view"}
+              bg={"transparent"}
+              iconName={"ri-admin-line"}
+              onClickFunction={() => setIsOpenModal(true)}
+              className={"my-8 mx-2 px-6 py-2 rounded-md font-bold border border-gray-400"}
+            />
+            <Button           
+              text={"Enter hackathon code"}
+              bg={"#ffa988"}
+              iconName={"ri-add-circle-line"}
+              className={"my-8 mx-2 px-6 py-2 rounded-md font-bold text-black"}
+            />
+          </div>
+        </div>
+        <div className="w-full">
+          <input 
+            className="w-full p-2 border rounded-lg text-white" 
+            type="text" 
+            placeholder="Search..."
           />
         </div>
-      </div>
-      <div className="w-full">
-        <input 
-          className="w-full p-2 border rounded-lg text-white" 
-          type="text" 
-          placeholder="Search..."
-        />
-      </div>
-      <div className="h-full w-full">
-        <div className="w-full my-10 grid grid-cols-3 gap-8">
-          <Card 
-            title={"Hackathon 1"}
-            description={"Hola soy una descripcion de prueba"}
-            code={"asd6893a"}
-          />
-          <Card 
-            title={"Hackathon 1"}
-            description={"Hola soy una descripcion de prueba"}
-            code={"asd6893a"}
-          />
-          <Card 
-            title={"Hackathon 1"}
-            description={"Hola soy una descripcion de prueba"}
-            code={"asd6893a"}
-          />
-          <Card 
-            title={"Hackathon 1"}
-            description={"Hola soy una descripcion de prueba"}
-            code={"asd6893a"}
-          />
-          <Card 
-            title={"Hackathon 1"}
-            description={"Hola soy una descripcion de prueba"}
-            code={"asd6893a"}
-          />
-          <Card 
-            title={"Hackathon 1"}
-            description={"Hola soy una descripcion de prueba"}
-            code={"asd6893a"}
-          />
+        <div className="h-full w-full">
+          <div className="w-full my-10 grid grid-cols-3 gap-8">
+            <Card 
+              title={"Hackathon 1"}
+              description={"Hola soy una descripcion de prueba"}
+              code={"asd6893a"}
+            />
+            <Card 
+              title={"Hackathon 1"}
+              description={"Hola soy una descripcion de prueba"}
+              code={"asd6893a"}
+            />
+            <Card 
+              title={"Hackathon 1"}
+              description={"Hola soy una descripcion de prueba"}
+              code={"asd6893a"}
+            />
+            <Card 
+              title={"Hackathon 1"}
+              description={"Hola soy una descripcion de prueba"}
+              code={"asd6893a"}
+            />
+            <Card 
+              title={"Hackathon 1"}
+              description={"Hola soy una descripcion de prueba"}
+              code={"asd6893a"}
+            />
+            <Card 
+              title={"Hackathon 1"}
+              description={"Hola soy una descripcion de prueba"}
+              code={"asd6893a"}
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
+
   )
 }
 
