36a2f1b198e73774db6bebafa638dca4ac96a188
Mariano Botto <marianobotto@gmail.com>
2024-03-13 15:50:11 -0300

    style - Front: Change bg color

--- front/src/pages/Home/Home.jsx
+++ front/src/pages/Home/Home.jsx
@@ -4,34 +4,36 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="container mx-auto">
-      <img
-        src="https://media.licdn.com/dms/image/C4E0BAQHZYYIUKBtZNw/company-logo_200_200/0/1677855312170/devtalles_logo?e=1718236800&v=beta&t=Mr5D2UCUBCFCNKu3VsgHf2tjlFbFgUCfrxKJoN4f1NQ"
-        className="logo my-3"
-      />
-      <h1 className="text-center text-white text-4xl">
-        Bienvenidos a FastRaffle
-      </h1>
-      <h2 className="text-center text-white text-3xl">
-        ¡La app de sorteos de DevTalles!
-      </h2>
-      <Link to={"/login"}>
-        <Button
-          text={"Comenzar"}
-          bg={"#ffa988"}
-          className={"my-8 px-10 py-2 rounded-md font-bold"}
+    <div className="bg-impresario">
+      <div className="container mx-auto">
+        <img
+          src="https://media.licdn.com/dms/image/C4E0BAQHZYYIUKBtZNw/company-logo_200_200/0/1677855312170/devtalles_logo?e=1718236800&v=beta&t=Mr5D2UCUBCFCNKu3VsgHf2tjlFbFgUCfrxKJoN4f1NQ"
+          className="logo my-3"
         />
-      </Link>
-      <div className="text-left border-gray-400 w-1/3 border px-3 py-3 rounded-md">
-        <h4 className="text-xl text-white pb-6">
-          <i className="ri-coupon-fill mr-2"></i>
-          Participa, comparte y disfruta!
-        </h4>
-        <p className="welcomeDivText">Gana muchos premios!</p>
-        <p className="welcomeDivText py-2">Sorteos de la comunidad</p>
-        <p className="welcomeDivText" style={{ marginBottom: 0 }}>
-          Premios varios...
-        </p>
+        <h1 className="text-center text-white text-4xl">
+          Bienvenidos a FastRaffle
+        </h1>
+        <h2 className="text-center text-white text-3xl">
+          ¡La app de sorteos de DevTalles!
+        </h2>
+        <Link to={"/login"}>
+          <Button
+            text={"Comenzar"}
+            bg={"#ffa988"}
+            className={"my-8 px-10 py-2 rounded-md font-bold"}
+          />
+        </Link>
+        <div className="text-left border-gray-400 w-1/3 border px-3 py-3 rounded-md">
+          <h4 className="text-xl text-white pb-6">
+            <i className="ri-coupon-fill mr-2"></i>
+            Participa, comparte y disfruta!
+          </h4>
+          <p className="welcomeDivText">Gana muchos premios!</p>
+          <p className="welcomeDivText py-2">Sorteos de la comunidad</p>
+          <p className="welcomeDivText" style={{ marginBottom: 0 }}>
+            Premios varios...
+          </p>
+        </div>
       </div>
     </div>
   );
--- front/src/pages/Login/Login.jsx
+++ front/src/pages/Login/Login.jsx
@@ -6,21 +6,23 @@ const Login = () => {
     window.location.href = "http://localhost:8080/api/auth/discord";
   };
   return (
-    <div className="container mx-auto">
-      <img
-        src="https://media.licdn.com/dms/image/C4E0BAQHZYYIUKBtZNw/company-logo_200_200/0/1677855312170/devtalles_logo?e=1718236800&v=beta&t=Mr5D2UCUBCFCNKu3VsgHf2tjlFbFgUCfrxKJoN4f1NQ"
-        className="logo my-3"
-      />
-      <h1 className="text-center text-white text-3xl">
-        Inicia sesión para continuar!
-      </h1>
-      <Button
-        onClickFunction={handleLogin}
-        text={"Continuar con Discord"}
-        iconName={"ri-discord-fill"}
-        bg={"#ffa988"}
-        className={"my-8 px-10 py-2 rounded-md font-bold"}
-      />
+    <div className="bg-impresario">
+      <div className="container mx-auto">
+        <img
+          src="https://media.licdn.com/dms/image/C4E0BAQHZYYIUKBtZNw/company-logo_200_200/0/1677855312170/devtalles_logo?e=1718236800&v=beta&t=Mr5D2UCUBCFCNKu3VsgHf2tjlFbFgUCfrxKJoN4f1NQ"
+          className="logo my-3"
+        />
+        <h1 className="text-center text-white text-3xl">
+          Inicia sesión para continuar!
+        </h1>
+        <Button
+          onClickFunction={handleLogin}
+          text={"Continuar con Discord"}
+          iconName={"ri-discord-fill"}
+          bg={"#ffa988"}
+          className={"my-8 px-10 py-2 rounded-md font-bold"}
+        />
+      </div>
     </div>
   );
 };
