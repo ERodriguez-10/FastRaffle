4d314c4a41913f94d62fa1a9ebcbdcfad1e57790
MEBotto <marianobotto92@gmail.com>
2024-03-15 16:59:38 -0300

    feature - back: Add post method

--- back/src/routes/user.routes.js
+++ back/src/routes/user.routes.js
@@ -4,3 +4,11 @@
  * @param {String} avatar_id The avatar id
  * Example: https://cdn.discordapp.com/avatars/969748548256149515/c07d6d073f124200875a15b4a65130b9.png
  */
+
+import { Router } from "express";
+
+userRouter = Router();
+
+userRouter.post("/login", async (req, res) => {
+  
+});
